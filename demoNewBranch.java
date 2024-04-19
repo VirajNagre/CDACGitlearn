ec82be51f3500c45e7be68a4c236e5ed11629a58
viraj <virajngr@gmail.com>
2024-04-19 08:49:54 +0530

    nested class added to the demo file

--- demoNewBranch.java
+++ demoNewBranch.java
@@ -1,5 +1,13 @@
 class DemoNewBranch{
     public static void main(String[] args) {
         System.out.println("okok, file created in new  brnach");
+        Nested n = new Nested();
+    }
+
+}
+class Nested{
+    int data;
+    Nested(){
+        data=25;
     }
 }
